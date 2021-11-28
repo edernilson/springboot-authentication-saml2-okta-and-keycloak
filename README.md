a7d9f57f9628f8dde26d06680d5711459576b879
Eder Nilson <eder.nilson@gmail.com>
2021-11-28 10:03:21 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Boot Authentication SAML 2.0 with Keycloak and Okta
+
+### Tasks
+    [ ] Libs configuration
+    [ ] Keycloak configuration for new application
+    [ ] SAML connection for kecloak
+    [ ] Okta configuration for new application
+    [ ] Tests with new profile 
+
---
+++ src/main/java/com/edernilson/saml2/Saml2Application.java
@@ -0,0 +1,13 @@
+package com.edernilson.saml2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saml2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Saml2Application.class, args);
+	}
+
+}
